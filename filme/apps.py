a86ad291a36d54305afca8cd3587adae9e6f7f9e
LucasPadula-Twelve <lucas.padula@twelvesolutions.com.br>
2024-09-10 23:05:02 -0300

    tempo para criar o bd

--- filme/apps.py
+++ filme/apps.py
@@ -1,4 +1,5 @@
 from django.apps import AppConfig
+import time
 
 
 class FilmeConfig(AppConfig):
@@ -8,6 +9,7 @@ class FilmeConfig(AppConfig):
     def ready(self):
         from .models import Usuario
         import os
+        time.sleep(4)
         email = os.getenv("EMAIL_ADMIN")
         senha = os.getenv("SENHA_ADMIN")
         usuarios = Usuario.objects.filter(email=email)
